b9cfbcd2d208e17ce23c6dc994dafeeea3189671
曹宇 <1481619987@qq.com>
2018-05-30 14:22:35 +0000

    会话控制

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,17 @@
 |
 */
 
- Route::get('/','StaticPagesController@home')->name('home');
- Route::get('/help','StaticPagesController@help')->name('help');
- Route::get('/about','StaticPagesController@about')->name('about');
+Route::get('/','StaticPagesController@home')->name('home');
+Route::get('/help','StaticPagesController@help')->name('help');
+Route::get('/about','StaticPagesController@about')->name('about');
  //注册
 Route::get('signup','UsersController@create')->name('signup');
 //接受注册数据
 Route::resource('users','UsersController');
+//会话控制
+//显示
+Route::get('login','SessionsController@create')->name('login');
+//登录
+Route::post('login','SessionsController@store')->name('login');
+//注销
+Route::delete('logout','SessionsController@destroy')->name('logout');
